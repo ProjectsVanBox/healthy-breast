5f18f79d5882a1e6d18b02383135e5bb6110a7ea
Elizaveta Kulaeva <86182915+lizakulaeva@users.noreply.github.com>
2025-08-18 14:30:01 +0200

    Update subset_by_PTATO_output_analysis.sh

--- subset_by_PTATO_output_analysis.sh
+++ subset_by_PTATO_output_analysis.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=e.kulaeva@prinsesmaximacentrum.nl
 
-home_dir = $1
+home_dir=$1
 
 ## Breast_1
 ### merge all PTATO-filtered vcfs
@@ -13,13 +13,13 @@ home_dir = $1
 module load bcftools
 module load R
 
-cd $home_dir/3_Output/Breast_1/filtered_PTA/PTATO_PON/snvs/Breast_1/
+cd "$home_dir/3_Output/Breast_1/filtered_PTA/PTATO_PON/snvs/Breast_1/"
 bcftools merge -Oz PTA_WGS_0.2_PON_S8506Nr3/PTA_WGS_0.2_PON_S8506Nr3.snvs.ptato.filtered.vcf.gz PTA_WGS_0.2_PON_S8506Nr4/PTA_WGS_0.2_PON_S8506Nr4.snvs.ptato.filtered.vcf.gz PTA_WGS_0.2_PON_S8506Nr5/PTA_WGS_0.2_PON_S8506Nr5.snvs.ptato.filtered.vcf.gz PTA_WGS_0.2_PON_S8506Nr6/PTA_WGS_0.2_PON_S8506Nr6.snvs.ptato.filtered.vcf.gz > $home_dir/3_Output/Breast_1/filtered_PTA/downstream/PTATO_filtered.vcf.gz
 bcftools merge -Oz PTA_WGS_0.2_PON_S8506Nr3.snvs.ptato.vcf.gz PTA_WGS_0.2_PON_S8506Nr4.snvs.ptato.vcf.gz PTA_WGS_0.2_PON_S8506Nr5.snvs.ptato.vcf.gz PTA_WGS_0.2_PON_S8506Nr6.snvs.ptato.vcf.gz >  $home_dir/3_Output/Breast_1/filtered_PTA/downstream/PTATO_flagged.vcf.gz
 
 ### filter out dbSNPs from the filtered file and subset initial multiVCF file by PTATO filtered variants
 
-cd $home_dir/3_Output/Breast_1/filtered_PTA/downstream/
+cd "$home_dir/3_Output/Breast_1/filtered_PTA/downstream/"
 
 tabix -p vcf PTATO_filtered.vcf.gz
 tabix -p vcf PTATO_flagged.vcf.gz
