48230726b7d49c0924f7ddc6c04e4f7fae195ed5
Elizaveta Kulaeva <86182915+lizakulaeva@users.noreply.github.com>
2025-08-18 12:03:13 +0200

    Update PON_remove_PTA.R

--- PON_remove_PTA.R
+++ PON_remove_PTA.R
@@ -1,14 +1,14 @@
-rm(list=ls(all=TRUE))
-options(stringsAsFactors = FALSE)
-
 ### Load libraries
 library(UpSetR)
 library(dplyr)
 library(VariantAnnotation)
 library(stringr)
 
-vcf_file = './3_Output/Breast_1/filtered_PTA/downstream/final_filtered_0.2_biall.vcf.gz'
-vcf_pon = 'HMF_PON_POSITION'
+### pass home_dif
+args <- commandArgs(trailingOnly = TRUE)
+
+vcf_file = paste0(args[1],"/3_Output/Breast_1/filtered_PTA/downstream/final_filtered_0.2_biall.vcf.gz")
+vcf_pon = 'HMF_PON_LOCATION'
 
 vcf <- readVcf(vcf_file)
 
@@ -21,4 +21,4 @@ variants_to_exclude <- rownames(pon)
 # Exclude variants from the VCF file based on the variant IDs from PON
 vcf <- vcf[(!(rownames(vcf) %in% variants_to_exclude)), ]
 
-writeVcf(vcf, file = "./3_Output/Breast_1/filtered_PTA/Breast_1/PTA_WGS_0.2_PON.vcf")
+writeVcf(vcf, file = paste0(,args[1],"/3_Output/Breast_1/filtered_PTA/Breast_1/PTA_WGS_0.2_PON.vcf")
