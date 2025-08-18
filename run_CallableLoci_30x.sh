599f027f888975dfd151e544e2bc738bd5ee491e
Elizaveta Kulaeva <86182915+lizakulaeva@users.noreply.github.com>
2025-08-18 10:48:18 +0200

    Create run_CallableLoci_30x.sh

---
+++ run_CallableLoci_30x.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --time=24:0:0
+#SBATCH --mem=30G 
+
+echo "Start CallableLoci        " `date` "      " `uname -n`
+
+module load Java/1.8.0_60
+
+BAM=$1
+NAME=$(basename $BAM)
+BED=${NAME/.bam/_CallableLoci.bed}
+TXT=${NAME/.bam/_CallableLoci.txt}
+TMP=${NAME/.bam/_tmp}
+
+java -Djava.io.tmpdir=${TMP} \
+-Xmx30G -jar /hpc/local/CentOS7/pmc_vanboxtel/bin/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar -T CallableLoci \
+-R /hpc/pmc_vanboxtel/resources/homo_sapiens.GRCh38.GATK.illumina/Homo_sapiens_assembly38.fasta \
+-I ${BAM} \
+-o ${BED} \
+-summary ${TXT} \
+--minBaseQuality 10 --minMappingQuality 10 --minDepth 15 --minDepthForLowMAPQ 20 
+
+echo "Finished CallableLoci     " `date` "      " `uname -n`
