36ccf5ce5fe052d592a69474c7bdc776ea2b6deb
Elizaveta Kulaeva <86182915+lizakulaeva@users.noreply.github.com>
2025-08-18 10:57:39 +0200

    Update batch_PTA+WGS_analysis_1.sh

--- batch_PTA+WGS_analysis_1.sh
+++ batch_PTA+WGS_analysis_1.sh
@@ -3,15 +3,15 @@
 
 ## filtering common callable loci
 ### creating dirs
-mkdir .output/filtered_PTA/callable
-cd .output/filtered_PTA/callable
+mkdir 3_Output/filtered_PTA/callable
+cd 3_Output/filtered_PTA/callable
 
 ### run CallableLoci for WGS samples
-sbatch ./run_CallableLoci_30x.sh ./inputs/Breast/S8506Nr1_dedup.bam
-sbatch ./run_CallableLoci_30x.sh ./inputs/Breast/S8506Nr2_dedup.bam
+sbatch ./run_CallableLoci_30x.sh 1_Input/Breast_1/S8506Nr1_dedup.bam
+sbatch ./run_CallableLoci_30x.sh 1_Input/Breast_1/S8506Nr2_dedup.bam
 
 ### run CallableLoci for PTA samples
-sbatch ./run_CallableLoci_15x.sh ./inputs/Breast/S8506Nr3_dedup.bam
-sbatch ./run_CallableLoci_15x.sh ./inputs/Breast/S8506Nr4_dedup.bam
-sbatch ./run_CallableLoci_15x.sh ./inputs/Breast/S8506Nr5_dedup.bam
-sbatch ./run_CallableLoci_15x.sh ./inputs/Breast/S8506Nr6_dedup.bam
+sbatch ./run_CallableLoci_15x.sh 1_Input/Breast_1/S8506Nr3_dedup.bam
+sbatch ./run_CallableLoci_15x.sh 1_Input/Breast_1/S8506Nr4_dedup.bam
+sbatch ./run_CallableLoci_15x.sh 1_Input/Breast_1/S8506Nr5_dedup.bam
+sbatch ./run_CallableLoci_15x.sh 1_Input/Breast_1/S8506Nr6_dedup.bam
