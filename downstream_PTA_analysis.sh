39e410bd3373daeb9d69a2bdfd7afbe749df59b4
Elizaveta Kulaeva <86182915+lizakulaeva@users.noreply.github.com>
2025-08-19 11:19:37 +0200

    Update downstream_PTA_analysis.sh

--- downstream_PTA_analysis.sh
+++ downstream_PTA_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -t 6:0:0
 #SBATCH -c 6
-#SBATCH --mem=60G
+#SBATCH --mem=80G
 
 module load bcftools
 module load R
@@ -47,8 +47,7 @@ awk '{OFS="\t"; print $1,$2}' variants_02_filt_2.txt > pos_02_filt.tsv
 ### subset initial VCF by positions list and remove PON variants
 bcftools view -Oz -R pos_02_filt.tsv biall_snps.vcf.gz > final_filtered_0.2_biall.vcf.gz
 mkdir "$home_dir/3_Output/Breast_1/filtered_PTA/Breast_1/"
-bcftools query -f'%CHROM\t%POS0\t%END\n' dummy_PON.vcf > PON.bed ### modify this by adding the HMF panel of normals file location instead of dummy_PON.vcf
-bcftools view -T ^PON.bed -Oz -o "$home_dir/3_Output/Breast_1/filtered_PTA/Breast_1/PTA_WGS_0.2_PON.vcf.gz" final_filtered_0.2_biall.vcf.gz
+Rscript PON_remove_PTA.R $home_dir ### modify this script by adding the HMF panel of normals file location
 mkdir "$home_dir/3_Output/Breast_1/filtered_PTA/PTATO_PON"
 
 ### remove intermediate files (optional)
