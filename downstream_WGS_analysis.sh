ad5bbec93bcf2b1c3682998b1614af7d82607ab8
Elizaveta Kulaeva <86182915+lizakulaeva@users.noreply.github.com>
2025-08-19 12:57:24 +0200

    Update downstream_WGS_analysis.sh

--- downstream_WGS_analysis.sh
+++ downstream_WGS_analysis.sh
@@ -4,9 +4,20 @@
 #SBATCH --mem=50G
 
 home_dir= $1
-ASAP_ID=$2
+ASAP_NAME=$2
 
 ### Breast_2 analysis (WGS only)
 module load bcftools
 module load R
 
+cd "$home_dir/3_Output/Breast_2/ASAP/vcf/germline/somatic_filtering/SMuRF/"
+bgzip $ASAP_NAME.vep.SMuRF.filtered.joined.vcf
+
+mkdir "$home_dir/3_Output/Breast_2/filtered/downstream"
+cd "$home_dir/3_Output/Breast_2/filtered/downstream"
+
+bcftools view -Oz -f PASS -m2 -M2 -v snps $home_dir/3_Output/Breast_2/ASAP/vcf/germline/somatic_filtering/SMuRF/$ASAP_NAME.vep.SMuRF.filtered.joined.vcf.gz > biall_snps.vcf.gz
+bcftools sort -Oz biall_snps.vcf.gz > biall_snps_sorted.vcf.gz
+bcftools index biall_snps_sorted.vcf.gz
+
+Rscript mutmat_WGS.R $home_dir
